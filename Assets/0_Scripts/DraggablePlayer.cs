2d0ea39a1003523676722d88e4f6e6f58eb4dd40
Marvin Moreno <marmor0416@gmail.com>
2023-08-16 23:18:43 +0200

    aha

--- Assets/0_Scripts/DraggablePlayer.cs
+++ Assets/0_Scripts/DraggablePlayer.cs
@@ -6,9 +6,5 @@ using UnityEngine.EventSystems;
 
 public class DraggablePlayer : MonoBehaviour
 {
-    public void OnMouseDrag()
-    {
-        Vector3 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(newPos.x, newPos.y, 0f);
-    }
+    
 }
